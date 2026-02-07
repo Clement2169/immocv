46203560e86a2e97041224a802565e2abe7a5473
Clement2169 <cvdev21@gmail.com>
2026-02-07 11:15:44 +0100

    update data viz

--- notebooks/streamlit/streamlit_data_vis.py
+++ notebooks/streamlit/streamlit_data_vis.py
@@ -12,7 +12,7 @@ import matplotlib.ticker as mtick
 from config import *
 
 def corr_plots(df_num_corr,corr_type,labels,corr_threshold=0.1):
-    fig, ax = plt.subplots(figsize=(5, 7)) 
+    fig, ax = plt.subplots() 
     param = {
         'vmax': 1, 
         'vmin': -1, 
@@ -36,32 +36,36 @@ def DataViz() :
     title = "Visualization et traitement sur les données"
     st.write(title)
     
-    st.write('Description du dataset initial : ')  
-    filepath = os.path.join(data_dir_visu, 'prop_maison_app_plot.png')
-    st.image(filepath, )
-    st.table(pd.read_pickle(os.path.join(data_dir_visu,'DataSummaryTable.pkl')))
+    intial_tab, nan_tab,cor_tab = st.tabs(['Dataset initial', r"NAN dans le dataset","Corrélation des variables avec le prix/m²"])
+    with intial_tab :
+        st.write('Description du dataset initial : ')  
+        filepath = os.path.join(data_dir_visu, 'prop_maison_app_plot.png')
+        st.image(filepath, )
+        st.table(pd.read_pickle(os.path.join(data_dir_visu,'DataSummaryTable.pkl')))
+
+    with nan_tab :  
+        st.subheader(r"Figure montrant le % des NAN dans le dataset initial :")
+        filepath = os.path.join(data_dir_visu, 'percentage_NA_alldata_plot.png')
+        st.image(filepath, )
     
-    st.subheader(r"Figure montrant le % des NAN dans le dataset initial :")
-    filepath = os.path.join(data_dir_visu, 'percentage_NA_alldata_plot.png')
-    st.image(filepath, )
+    with cor_tab :  
+        st.subheader(r"Corrélation des variables avec le prix/m² :")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            house_flat = st.selectbox('Type de bien', HOUSE_FLAT_CHOICE,index=0)
+        with col2:
+            corr_type = st.selectbox('Type de corrélation', ['pearson','spearman'],index=0)
+        with col3:
+            corr_threshold = float(st.text_input("Seuil de corrélation (%)? ",value=10))
     
-    st.subheader(r"Corrélation des variables avec le prix/m² :")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        house_flat = st.selectbox('Type de bien', HOUSE_FLAT_CHOICE,index=0)
-    with col2:
-        corr_type = st.selectbox('Type de corrélation', ['pearson','spearman'],index=0)
-    with col3:
-        corr_threshold = float(st.text_input("Seuil de corrélation (%)? ",value=10))
-    
-    corr_file_path=os.path.join(data_dir_visu, f'corr_df_{house_flat}_{corr_type}.pkl')
-    df_num_corr=pd.read_pickle(corr_file_path)
-    
-    if st.button("Afficher"):
-        if house_flat == HOUSE_NAME :
-            corr_plots(df_num_corr,corr_type,house_labels,corr_threshold=corr_threshold/100)
-        elif house_flat == FLAT_NAME :
-            corr_plots(df_num_corr,corr_type,flat_labels,corr_threshold=corr_threshold/100)
+        corr_file_path=os.path.join(data_dir_visu, f'corr_df_{house_flat}_{corr_type}.pkl')
+        df_num_corr=pd.read_pickle(corr_file_path)
+        
+        if st.button("Afficher"):
+            if house_flat == HOUSE_NAME :
+                corr_plots(df_num_corr,corr_type,house_labels,corr_threshold=corr_threshold/100)
+            elif house_flat == FLAT_NAME :
+                corr_plots(df_num_corr,corr_type,flat_labels,corr_threshold=corr_threshold/100)
 
 
 
