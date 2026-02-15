fa1a8475f41ed5eddc0fd394ed458a2dd503a668
Clement2169 <cvdev21@gmail.com>
2026-02-15 18:29:29 +0100

    remove page_prediction_prix_flat(data_dir_prix old

--- notebooks/streamlit/streamlit_maison_app.py
+++ notebooks/streamlit/streamlit_maison_app.py
@@ -6,7 +6,19 @@ from config import FLAT_NAME,HOUSE_FLAT_CHOICE, HOUSE_NAME, load_model_file, loa
 FLAT_MODEL_NAME = "flat_model"
 HOUSE_MODEL_NAME= "house_model"
 
-def house_flat_encoding(df,dpe_name= None):
+def house_flat_encoding(df, model_name):
+        
+
+        if model_name == FLAT_MODEL_NAME :
+            energy_prefix  = "chauf_energy" 
+            ges_name = "ges_class_num"
+            dpe_name = 'dpeL_num'
+
+        else :
+            energy_prefix  = "chauf_energie" 
+            ges_name = "ges_class"
+            dpe_name = 'dpeL'
+
 
         #  *****************************************************************************
         #  Traitement_NA
@@ -23,13 +35,9 @@ def house_flat_encoding(df,dpe_name= None):
 
         mapping = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7 , 'F/G':7 , 'Unknown':7}
         if 'ges_class' in df.columns:
-            df['ges_class']=df['ges_class'].map(mapping)
+            df[ges_name]=df['ges_class'].map(mapping)
         if 'dpeL' in df.columns:
-            if dpe_name is not None :
-                df[dpe_name]=df['dpeL'].map(mapping)
-                df = df.drop("dpeL",axis=1)
-            else:
-                df['dpeL']=df['dpeL'].map(mapping)
+            df[dpe_name]=df['dpeL'].map(mapping)
 
         #  *****************************************************************************
         #  chauffage_energie
@@ -39,7 +47,7 @@ def house_flat_encoding(df,dpe_name= None):
         if 'chauffage_energie' in df.columns:
             df["chauff_energie_encoded"]=df['chauffage_energie'].apply(lambda x : 'combined' if type(x)==list else x)
             #pd.dummies
-            df=df.merge(pd.get_dummies(df["chauff_energie_encoded"],prefix='chauf_energie',dtype='int'),how='left', left_index=True, right_index=True)
+            df=df.merge(pd.get_dummies(df["chauff_energie_encoded"],prefix=energy_prefix,dtype='int'),how='left', left_index=True, right_index=True)
             df.drop(columns=['chauffage_energie','chauff_energie_encoded'],inplace=True)
 
 
@@ -72,14 +80,14 @@ def house_flat_add_ACP(df,pca):
         return df
 
 import streamlit as st
-def house_input_prep(input_house,box_names,pca,dpe_name=None):
+def house_input_prep(input_house,box_names,pca,model_name):
     df_house_pred=pd.DataFrame([input_house])
     df_house_pred[box_names] = df_house_pred[box_names].apply(pd.to_numeric, errors='coerce')
     
     st.write("DF avant encodage : ")
     st.dataframe(df_house_pred)
     
-    df_house_encoded=house_flat_encoding(df_house_pred,dpe_name)
+    df_house_encoded=house_flat_encoding(df_house_pred,model_name)
     df_house_encoded=house_flat_exposition_streamlit(df_house_encoded)
     
     st.write("DF apres encodage : ")
@@ -250,11 +258,11 @@ def page_prediction_prix_house(data_dir_prix):
                 'nb log n7',  'loyer m2 median n7', 'taux rendement n7']
     
     box_ids_default=None
-    is_chauffage=True
     columns_to_exclude = ['nb_log_n7',  'loyer_m2_median_n7', 'taux_rendement_n7']
+    energies=['radiateur','sol' ,'pompe à chaleur','climatisation révérsible','convecteur','poêle à bois','cheminée','chaudière']    
 
 
-    page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_names,box_ids_default, box_names,is_chauffage,columns_to_exclude)
+    page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_names,box_ids_default, box_names,energies,columns_to_exclude)
 
 
 
@@ -294,7 +302,7 @@ def flat_input_prep(input_flat,box_names,pca):
 #  page_prediction_prix_flat
 #  *****************************************************************************
 
-def page_prediction_prix_flat_new(data_dir_prix):
+def page_prediction_prix_flat(data_dir_prix):
 
     model_name = FLAT_MODEL_NAME
     if model_name not in st.session_state :
@@ -303,155 +311,27 @@ def page_prediction_prix_flat_new(data_dir_prix):
         st.session_state[model_name]=final_model
     
     box_ids=['logement_neuf',  'surface', 'annee_construction' , 'nb_pieces','nb_etages',   
-               'nb_places_parking', 'nb_logements_copro','charges_copro_m2',  'ascenceur', 'porte_digicode']
+               'nb_places_parking', 'nb_logements_copro','charges_copro_m2',  'ascenseur', 'porte_digicode','cave']
     
     box_ids_names=['logement neuf',  'surface', 'annee construction' , 'nb pieces','nb etages',   
-               'nb places parking', 'nb logements copro','charges copro m2',  'ascenceur', 'porte digicode']
+               'nb places parking', 'nb logements copro','charges copro m2',  'ascenseur', 'porte digicode','cave']
     
-    box_ids_default=[0,  60, 2010 , 4,6, 1,25,2000, 1,1]
+    box_ids_default=[0,  60, 2010 , 4,6, 1,25,2000, 1,1,0]
     
     box_names= box_ids.copy()
     box_names.extend(['DEP', 'REG','UU2010','CODE_IRIS'])
     columns_to_exclude = [['nb_log_n7',  'loyer_m2_median_n7', 'taux_rendement_n7']]
-
-    page_prediction_prix_commun(data_dir_prix,model_name,box_ids, box_ids_names, box_ids_default,box_names,False,columns_to_exclude,"dpeL_num")
-
-
-#  *****************************************************************************
-#  page_prediction_prix_flat
-#  *****************************************************************************
-
-def page_prediction_prix_flat(data_dir_prix):
+    energies = ["radiateur","pompe à chaleur","convecteur","climatisation","chaudière","sol","poêle-bois"]
 
 
-    input_house={}
+    page_prediction_prix_commun(data_dir_prix,model_name,box_ids, box_ids_names, box_ids_default,box_names,energies,columns_to_exclude)
 
-    model_name = FLAT_MODEL_NAME
-    if model_name not in st.session_state :
-        filename = "appartement_model.gz"
-        final_model = load_model_file(data_dir_prix,filename)
-        st.session_state[model_name]=final_model
-
-    pca = st.session_state["pca"]
-    final_model=st.session_state[model_name]
-    info_geo = st.session_state["reference_iris"]
-
-
-    box_ids=['logement_neuf',  'surface', 'annee_construction' , 'nb_pieces','nb_etages',   
-               'nb_places_parking', 'nb_logements_copro','charges_copro_m2',  'ascenceur', 'porte_digicode']
-    
-    box_ids_names=['logement neuf',  'surface', 'annee construction' , 'nb pieces','nb etages',   
-               'nb places parking', 'nb logements copro','charges copro m2',  'ascenceur', 'porte digicode']
-    
-    box_ids_default=[0,  60, 2010 , 4,6, 1,25,2000, 1,1]
-    
-    box_names= box_ids.copy()
-    box_names.extend(['DEP', 'REG','UU2010','CODE_IRIS'])
-
-       # Créez 3 colonnes
-    col1, col2, col3 = st.columns(3)
-
-    # Remplir la première colonne avec des inputs
-    x=(len(box_ids))/3
-    with col1:
-        input_house['DEP'] = st.text_input(f'DEP',value =78)
-        for i, name in enumerate(box_ids):
-            if i // x == 0:  # pour s'assurer que chaque colonne a un certain nombre d'inputs
-                input_house[name] = st.text_input(f'{box_ids_names[i]}',value=box_ids_default[i])
-    # Remplir la deuxième colonne avec des inputs
-    with col2:
-        input_house['LIBCOM'] = st.selectbox('Commune',info_geo[info_geo['DEP']==input_house['DEP']]['LIBCOM'].unique(),index=1)
-        for i, name in enumerate(box_ids):
-            if i // x == 1:
-                input_house[name] = st.text_input(f'{box_ids_names[i]}',value=box_ids_default[i])
-    # Remplir la troisième colonne avec des inputs
-    with col3:
-        input_house['LIB_IRIS'] = st.selectbox('Quartier',info_geo[info_geo['LIBCOM']==input_house['LIBCOM']]['LIB_IRIS'].unique(),index=6)
-        for i, name in enumerate(box_ids):
-
-            if i // x == 2:
-                input_house[name] = st.text_input(f'{box_ids_names[i]}',value=box_ids_default[i])
-    
-    
-    # Sélection DPE et GES
-    dep_choices = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    col4, col5,col6 = st.columns(3)
-    with col4:
-        expo_choices=['nord', 'sud', 'est', 'ouest']    
-        input_house['expo'] = st.selectbox('Exposition', expo_choices,index=0)  
-    with col5:
-        input_house['dpeL'] = st.selectbox('dpeL', dep_choices, index=0)
-    with col6:
-        input_house['ges_class'] = st.selectbox('ges_class', dep_choices, index=0) 
-
-        
-    house_filter_geo = ((info_geo['DEP']==input_house['DEP']) &
-                    (info_geo['LIBCOM']==input_house['LIBCOM']) & 
-                    (info_geo['LIB_IRIS']==input_house['LIB_IRIS']))
-    filtered_data = info_geo[house_filter_geo]
-    if not filtered_data.empty:
-        # Récupérer les valeurs de la première ligne du DataFrame filtré
-        input_house['REG'] = int(filtered_data['REG'].values[0])
-        input_house['DEP'] = int(filtered_data['DEP'].values[0])
-        input_house['UU2010'] = int(filtered_data['UU2020'].values[0])
-        input_house['CODE_IRIS'] = int(filtered_data['CODE_IRIS'].values[0])
-    
-    
-    st.text("")
-    st.text("")
-    st.text("")
-    with st.expander("Afficher le résumé des informations du bien",expanded=False):
-        st.write("",input_house)
-    st.text("")
-    st.text("")
-    st.text("")
-
-    # Supprimer plusieurs clés
-    keys_to_remove = ["LIB_IRIS", "LIBCOM"]
-    LIBCOM=input_house['LIBCOM']
-    LIB_IRIS=input_house['LIB_IRIS']
-    for key in keys_to_remove:
-        input_house.pop(key, None)  # Utiliser `None` pour éviter une erreur si la clé n'existe pas
-
-
-    if st.button("Lancer la prédiction 🎯 "):
-        with st.expander("Afficher les étapes intermédiares de calcul",expanded=False):
-        
-            df_house_encoded=house_input_prep(input_house,box_names,pca,"dpeL_num")
-            # Faire une prédiction
-            columns_to_exclude = ['nb_log_n7',  'loyer_m2_median_n7', 'taux_rendement_n7']
-            df_encoded_reindexed , prediction = house_flat_price_pred(df_house_encoded,final_model,columns_to_exclude)
-            st.session_state.prediction = prediction
-        
-        st.subheader(f" Le prix/m² estimé est de : ⭐ { st.session_state.prediction[0]:.0f} € ")
-
-        # Generate SHAP waterfall plot for the prediction
-        shap_plot = generate_shap_waterfall_plot(final_model, df_encoded_reindexed)
-        
-        # Display the SHAP Waterfall Plot
-        st.pyplot(shap_plot)
-        
-        # thermometre de prix
-        
-        st.write(f'##### Comparaison avec la commune "{LIBCOM}"')
-        stat_path=os.path.join(data_dir_prix,f'stat_COM_{FLAT_NAME}.parquet')
-        stat=pd.read_parquet(stat_path)
-        stat=stat[stat['LIBCOM']==LIBCOM]
-        plot_simple_thermometer(st.session_state.prediction[0], stat['min'].values[0], stat['max'].values[0], stat['mean'].values[0])
-
-        st.write(f"##### Comparaison dans l'IRIS \"{LIB_IRIS}\"")
-        stat_path=os.path.join(data_dir_prix,f'stat_IRIS_{FLAT_NAME}.parquet')
-        stat=pd.read_parquet(stat_path)
-        stat=stat[stat['CODE_IRIS']==input_house['CODE_IRIS']]
-        plot_simple_thermometer(st.session_state.prediction[0], stat['min'].values[0], stat['max'].values[0], stat['mean'].values[0])
-    else:
-        st.write("Cliquez sur le bouton pour calculer la prediction du  prix / m² avec Explication SHAP")
 
 #  *****************************************************************************
 #  page_prediction_prix_commun
 #  *****************************************************************************
 
-def page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_names,box_ids_default, box_names,is_chauffage,columns_to_exclude,dpe_name=None):
+def page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_names,box_ids_default, box_names,energies,columns_to_exclude):
     
     
     info_geo  = st.session_state["reference_iris"]
@@ -504,14 +384,12 @@ def page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_nam
     with col6:
         input_house['ges_class'] = st.selectbox('ges_class', dep_choices, index=0) 
 
-    if is_chauffage :
-        col7, col8 = st.columns(2)
-        with col7:
-            chauffage_energie_choices=['elec','gaz','fioul','bois']
-            input_house['chauffage_energie'] = st.selectbox('chauffage_energie', chauffage_energie_choices,index=0)
-        with col8:
-            chauffage_systeme_choices=['radiateur','sol' ,'pompe à chaleur','climatisation révérsible','convecteur','poêle à bois','cheminée','chaudière']    
-            input_house['chauffage_systeme'] = st.selectbox('chauffage_systeme', chauffage_systeme_choices,index=0)
+    col7, col8 = st.columns(2)
+    with col7:
+        chauffage_energie_choices=['elec','gaz','fioul','bois']
+        input_house['chauffage_energie'] = st.selectbox('chauffage_energie', chauffage_energie_choices,index=0)
+    with col8:
+        input_house['chauffage_systeme'] = st.selectbox('chauffage_systeme', energies,index=0)
         
 
         
@@ -546,7 +424,7 @@ def page_prediction_prix_commun (data_dir_prix, model_name, box_ids, box_ids_nam
     if st.button("Lancer la prédiction 🎯 "):
         with st.expander("Afficher les étapes intermédiares de calcul",expanded=False):
         
-            df_house_encoded=house_input_prep(input_house,box_names,pca,dpe_name)
+            df_house_encoded=house_input_prep(input_house,box_names,pca,model_name)
             # Faire une prédiction
             df_encoded_reindexed , prediction = house_flat_price_pred(df_house_encoded,final_model,columns_to_exclude)
             st.session_state.prediction = prediction
@@ -597,7 +475,7 @@ def page_prediction_prix(data_dir_prix):
     if house_flat == HOUSE_NAME :
         page_prediction_prix_house(data_dir_prix)
     else :
-        page_prediction_prix_flat_new(data_dir_prix)
+        page_prediction_prix_flat(data_dir_prix)
 
 
 
